c1f0c04d7960d4e5bd35986a979679f94c356800
Penpinch <brandontorresr747@gmail.com>
2026-04-22 13:08:47 -0600

    Add render files.

---
+++ include/render.hpp
@@ -0,0 +1,7 @@
+# ifndef RENDER
+# define RENDER
+
+
+
+
+# endif
---
+++ src/render.cpp
@@ -0,0 +1,7 @@
+// Modificar update() dentro del archivo core.cpp para que update() 
+// solo sean consecuancias del juego, es decir, la deteccion de teclas dentro de update()
+// causa una acción en el juego, mas no es una consecuencia. Una consecuancia es por ejemplo
+// la actualizacion del puntaje.
+
+// Aqui irá Raylib
+# include "raylib.h"
